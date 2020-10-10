4efe45d525cebc0bebaba9cedf8e3d9fc1192a05
SunwooWonDigipen <sunwoo.won@digipen.edu>
2020-10-10 16:09:40 +0900

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,6 +1,26 @@
+let s;
 function setup() {
-  createCanvas(400, 400);
-
+  createCanvas(670, 570);
+  s = new ship();
+}
 function draw() {
-  background(220);
+  background(50);
+  s.update();
+  s.display();
+}
+class ship {
+    constructor() {
+        this.x = width/2;
+        this.y = height*0.8;
+    }
+    update() {
+        if(keyIsDown(LEFT_ARROW))
+            this.x-=5;
+        if (keyIsDown(RIGHT_ARROW))
+            this.x+=5;
+    }
+    display() {
+        fill(255);
+        circle(this.x,this.y,20,20);
+    }
 }
